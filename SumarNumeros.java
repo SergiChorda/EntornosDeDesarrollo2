261b66e04b0ab64097baca8cbceaa2089167690b
SergiChorda <sergichorda@hotmail.com>
2023-01-23 18:54:36 +0100

    Update SumarNumeros.java

---
+++ SumarNumeros.java
@@ -0,0 +1,11 @@
+public class SumarNumeros{
+    public static double sumerNumeros(double x, double y){
+        return x+y;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(SumarNumeros.sumerNumeros(2, 10.2));
+        
+    }
+
+}
