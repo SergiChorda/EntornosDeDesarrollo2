9f529300a36a904836c91a5ca8c08f14a671efa3
SergiChorda <sergichorda@hotmail.com>
2023-01-23 19:21:22 +0100

    Update SumarNumeros.java

--- SumarNumeros.java
+++ SumarNumeros.java
@@ -7,7 +7,7 @@ public class SumarNumeros {
     public static void main(String[] args) {
         // asi va el main
         // seguent linea de prova
-        // System.out.println("Hola mundo");
+        // System.out.println
         System.out.println(SumarNumeros.sumerNumeros(2, 10.2));
 
     }
