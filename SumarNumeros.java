0d0d8f248cc19c6e50e643434ce1a4aebedacd44
martaesc <escorihuela_m246@ieseduardoprimo.es>
2023-01-23 19:16:59 -0800

    Update SumarNumeros.java

--- SumarNumeros.java
+++ SumarNumeros.java
@@ -1,14 +1,15 @@
-public class SumarNumeros{
-    public static double sumerNumeros(double x, double y){
-        //asi es fa la suma
-        return x+y;
+public class SumarNumeros {
+    public static double sumerNumeros(double x, double y) {
+        // asi es fa la suma
+        return x + y;
     }
 
     public static void main(String[] args) {
-        //asi va el main
-        //seguent linea de prova
+        // asi va el main
+        // seguent linea de prova
+        // System.out.println("Hola mundo");
         System.out.println(SumarNumeros.sumerNumeros(2, 10.2));
-        
+
     }
 
 }
